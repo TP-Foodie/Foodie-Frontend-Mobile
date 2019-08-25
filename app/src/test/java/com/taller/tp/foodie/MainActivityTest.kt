20fd07a2d6a6553a3f5cecb5b868a37c8102f37d
Facundo Gerstner <fgerstner@devartis.com>
2019-08-25 19:08:52 -0300

    update travis ci config

--- app/src/test/java/com/taller/tp/foodie/MainActivityTest.kt
+++ app/src/test/java/com/taller/tp/foodie/MainActivityTest.kt
@@ -1,7 +1,7 @@
 package com.taller.tp.foodie
 
 import android.widget.TextView
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric.buildActivity
