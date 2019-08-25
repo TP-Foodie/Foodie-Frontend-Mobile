6aff4e9941e288bd641586311f87207de1240e15
Facundo Gerstner <fgerstner@devartis.com>
2019-08-25 17:30:00 -0300

    add first test

--- app/src/main/java/com/taller/tp/foodie/MainActivity.kt
+++ app/src/main/java/com/taller/tp/foodie/MainActivity.kt
@@ -14,13 +14,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
-    }
 }
