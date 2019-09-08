0f04ca2cafe67675c638d3d5af3210fd21d43f90
Facundo Gerstner <fgerstner@devartis.com>
2019-09-08 16:33:40 -0300

    add register screen layout

--- app/src/main/java/com/taller/tp/foodie/ui/RegisterActivity.kt
+++ app/src/main/java/com/taller/tp/foodie/ui/RegisterActivity.kt
@@ -9,5 +9,6 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar!!.hide()
     }
 }
