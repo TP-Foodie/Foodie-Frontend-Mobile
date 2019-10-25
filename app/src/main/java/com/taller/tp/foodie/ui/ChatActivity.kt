380380c4b79b9cea9da2e34244b327613e183b62
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2019-10-25 15:36:46 -0300

    agrego chat activity con su ui

---
+++ app/src/main/java/com/taller/tp/foodie/ui/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.taller.tp.foodie.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.taller.tp.foodie.R
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
